eee9c01890857a89dc2777eb71703b00e8d02f44
Drew Fargo <drew.fargo@gmail.com>
2024-09-21 16:43:12 -0500

    Implement Surrounded

--- src/main/java/edu/grinnell/csc207/blocks/Surrounded.java
+++ src/main/java/edu/grinnell/csc207/blocks/Surrounded.java
@@ -4,7 +4,7 @@ package edu.grinnell.csc207.blocks;
  * A text block surrounded by a single letter.
  *
  * @author Samuel A. Rebelsky
- * @author Your Name Here
+ * @author Andrew N. Fargo
  */
 public class Surrounded implements AsciiBlock {
   // +--------+------------------------------------------------------------
@@ -54,7 +54,13 @@ public class Surrounded implements AsciiBlock {
    *   If the row is invalid.
    */
   public String row(int i) throws Exception {
-    throw new Exception("Not yet implemented"); // STUB
+    if (i == 0 || i == this.height() - 1) {
+      return surroundChar.repeat(this.width());
+    } else if (i < this.height() - 1 && i > 0) {
+      return surroundChar + this.contents.row(i - 1) + surroundChar;
+    } else {
+      throw new Exception("Invalid row: " + i);
+    } // if
   } // row(int)
 
   /**
@@ -63,7 +69,7 @@ public class Surrounded implements AsciiBlock {
    * @return the number of rows
    */
   public int height() {
-    return 0;   // STUB
+    return this.contents.height() + 2;
   } // height()
 
   /**
@@ -72,7 +78,7 @@ public class Surrounded implements AsciiBlock {
    * @return the number of columns
    */
   public int width() {
-    return 0;   // STUB
+    return this.contents.width() + 2;
   } // width()
 
   /**
@@ -85,6 +91,19 @@ public class Surrounded implements AsciiBlock {
    *    false otherwise.
    */
   public boolean eqv(AsciiBlock other) {
-    return false;       // STUB
+    return (other instanceof Surrounded) && this.eqv((Surrounded) other);
   } // eqv(AsciiBlock)
+
+  /**
+   * Determine if another Surrounded block is structurally
+   * equivalent to this block.
+   * @param other
+   *   The Surrounded block to compare to this block.
+   * @return true if the two blocks are structurally equivalent and
+   *    false otherwise.
+   */
+  public boolean eqv(Surrounded other) {
+    return this.surroundChar == other.surroundChar
+      && this.contents.eqv(other.contents);
+  } // eqv(Surrounded)
 } // class Surrounded
