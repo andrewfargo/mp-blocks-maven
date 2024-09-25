3d2d8ac1e1c1bff0a4ac52aa130d6d7a8d9dcb61
Drew Fargo <drew.fargo@gmail.com>
2024-09-24 23:20:20 -0500

    Created first test

--- src/test/java/edu/grinnell/csc207/TestNewBlock.java
+++ src/test/java/edu/grinnell/csc207/TestNewBlock.java
@@ -1,6 +1,11 @@
 package edu.grinnell.csc207;
 
 import edu.grinnell.csc207.blocks.DropShadow;
+import edu.grinnell.csc207.blocks.HAlignment;
+import edu.grinnell.csc207.blocks.Line;
+import edu.grinnell.csc207.blocks.Rect;
+import edu.grinnell.csc207.blocks.VAlignment;
+import edu.grinnell.csc207.blocks.Boxed;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -14,11 +19,64 @@ public class TestNewBlock {
   // +-------+
 
   /**
-   * A placeholder.
+   * Test lines with shadows in every direction.
    */
   @Test
-  public void placeholder() {
-    assertEquals("hello", "hello");
-  } // placeholder()
+  public void testShadowLine() {
+    Line hello = new Line("Hello, world!");
+    assertEquals(""
+		 + "Hello, world!#\n"
+		 + "##############\n",
+		 new DropShadow(hello, VAlignment.BOTTOM, HAlignment.RIGHT));
+  } // testShadowLine
+
+  /**
+   * Test changed lines with shadows.
+   */
+  @Test
+  public void testShadowChangedLine() {
+
+  } // testShadowChangedLine
+
+  /**
+   * Test lines with boxes in every direction.
+   */
+  @Test
+  public void testShadowBoxes() {
+
+  } // testShadowBoxes
+
+  /**
+   * Test shadows with changed rectangles.
+   */
+  @Test
+  public void testShadowChangedRect() {
+
+  }
+  
+  /**
+   * Test shadows with composition.
+   */
+  @Test
+  public void testShadowComposites() {
+
+  } // testShadowComposites
+  
+  /**
+   * Test nested shadows.
+   */
+  @Test
+  public void testNestedShadows() {
+
+  } // testNestedShadows()
+
+  /**
+   * Test shadows of empties.
+   */
+  @Test
+  public void testShadowEmpty() {
+
+  } // testShadowEmpty()
+  
 
 } // class TestNewBlock
